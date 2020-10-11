03e89abe5c02523b56f8850d402b8fbce504f4df
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-10-11 18:30:59 +0200

    Extract main() function

--- upload.py
+++ upload.py
@@ -4,21 +4,27 @@ import os
 from dotenv import load_dotenv
 from mwapi import Session
 
-load_dotenv()
 
-username = os.getenv('MW_USERNAME')
-password = os.getenv('MW_PASSWORD')
-if not username or not password:
-    raise Exception('Username or password not provided!')
+def main():
+    load_dotenv()
 
-user_agent = 'We Are Beautiful upload (mail@lucaswerkmeister.de)'
-session = Session('https://commons.wikimedia.org',
-                  user_agent=user_agent)
-lgtoken = session.get(action='query',
-                      meta='tokens',
-                      type='login')['query']['tokens']['logintoken']
-session.post(action='login',
-             lgname=username,
-             lgpassword=password,
-             lgtoken=lgtoken)
-# TODO everything else :)
+    username = os.getenv('MW_USERNAME')
+    password = os.getenv('MW_PASSWORD')
+    if not username or not password:
+        raise Exception('Username or password not provided!')
+
+    user_agent = 'We Are Beautiful upload (mail@lucaswerkmeister.de)'
+    session = Session('https://commons.wikimedia.org',
+                      user_agent=user_agent)
+    lgtoken = session.get(action='query',
+                          meta='tokens',
+                          type='login')['query']['tokens']['logintoken']
+    session.post(action='login',
+                 lgname=username,
+                 lgpassword=password,
+                 lgtoken=lgtoken)
+    # TODO everything else :)
+
+
+if __name__ == '__main__':
+    main()
