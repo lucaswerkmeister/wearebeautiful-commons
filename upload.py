3953539000278ae0409f57f4f43d5528f4a9b662
Lucas Werkmeister <mail@lucaswerkmeister.de>
2020-10-11 19:29:53 +0200

    Add first version of upload() function
    It’s not called yet, and I haven’t tested it, but I hope it will work.
    It’s loosely based on the Python chunked upload sample code in the API
    documentation [1], but without special handling of the first chunk. We
    don’t know the file size in advance (the source files are gzipped, and I
    plan to decompress them on-the-fly), so we send a fake file size to the
    API; as far as I can tell from looking at ApiUpload.php, the API doesn’t
    check that it’s the same for each upload in a chunk, the only important
    thing is that the last chunk is marked by the total bytes being equal to
    the file size in that request.
    
    read_chunks() has a special case for ensuring that even if the input
    file happens to fit exactly into the last chunk, it sends one more empty
    chunk, which should cause upload() to send the final request where the
    file size matches the total bytes. However, I haven’t tested it, and
    given that upload() picks a chunk size of one mebibyte, this case is
    exceedingly unlikely to happen.
    
    [1]: https://www.mediawiki.org/wiki/API:Upload#Sample_code_3

--- upload.py
+++ upload.py
@@ -5,6 +5,55 @@ from dotenv import load_dotenv
 from mwapi import Session
 
 
+def read_chunks(file_object, chunk_size):
+    while data := file_object.read(chunk_size):
+        aligned = len(data) == chunk_size
+        yield data
+    if aligned:
+        # yield a final empty chunk to signal end-of-file,
+        # so that we still send an action=upload request where
+        # filesize == offset + len(chunk)
+        yield ''
+
+
+def upload(session, file_object, file_name, wikitext, comment):
+    chunk_size = 1024 * 1024
+    chunks = read_chunks(file_object, chunk_size)
+    token = session.get(action='query',
+                        meta='tokens')['query']['tokens']['csrftoken']
+    offset = 0
+    file_key = None
+
+    for chunk in chunks:
+        params = {
+            'action': 'upload',
+            'stash': '1',
+            'filename': file_name,
+            'offset': offset,
+            'token': token,
+            'upload_file': chunk,
+        }
+        if file_key:
+            params['filekey'] = file_key
+        if len(chunk) < chunk_size:
+            # this is the last chunk
+            params['filesize'] = offset + len(chunk)
+        else:
+            # we don’t know the filesize,
+            # so so just say “we’re not done yet”
+            params['filesize'] = offset + len(chunk) + 1
+        response = session.post(**params)
+        offset = response['upload']['offset']
+        file_key = response['upload']['filekey']
+
+    session.post(action='upload',
+                 filename=file_name,
+                 filekey=file_key,
+                 token=token,
+                 comment=comment,
+                 text=wikitext)
+
+
 def main():
     load_dotenv()
 
